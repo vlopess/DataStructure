90d7c0ddb83d8c263db6bc0efd1ed06bea5cab1d
VLopes <victorlopes9291@gmail.com>
2023-04-01 12:03:04 -0300

    erros na q1

--- INF006/Lista0_ALG_ADS/L0Q1/RL0Q1.c
+++ INF006/Lista0_ALG_ADS/L0Q1/RL0Q1.c
@@ -20,50 +20,50 @@ int main(){
   char file[255];  
   
   while(fgets(file, 255, fp_in) != NULL){
+    fflush(stdin);
     char splitHere[] = " (,)";
     char point[] = "points";    
     char text[20];
     int i, j, c, k;
     float distance, shortcut;
     int number[100];
-    int sort[100];
     float noSort[100];
     float distances[100];
-    char copy[255];
     i = j = c = k = 0;    
     distance = shortcut = 0;
     //points (−2,−1) (4,2) (4,0) (2,2) (8,6)
     char *p = strtok(file, point);
     // printf("%s\n", p);
-    char *slice  =  strtok(p, splitHere);    
+    char *slice  =  strtok(p, splitHere);
+       
     while (slice != NULL){
-      strcpy(copy, slice);
       number[i] =  atoi(slice);
       i++;     
       slice  =  strtok(NULL, splitHere);         
     }   
     i--;
-    for(int c = 0; c <= i - 3;c = c + 2){
+    for(int c = 0; c <= i && c < i - 1;c = c + 2){
       distance+= distanceCalc(number[c], number[c + 1], number[c + 2], number[c + 3]);      
     }
-    for(int c = 0; c < i - 2;c = c + 2){      
+    printf("%d\n", i);  
+    for(int c = 0; c < i;c += 2){      
       distances[j] = distanceCalc(number[c], number[c + 1], 0, 0); 
       noSort[j] = distanceCalc(number[c], number[c + 1], 0, 0); 
       j++;
     }
-    
     insertionSort(distances, j);
-    
-    // for(int c = 0; c<j ;c++){
-    //   printf("%2.2f\t\t\t", distances[c]);
-    //   printf("%2.2f\n", noSort[c]);
-    // }
+    printf("%d\n", j);  
+    for(int c = 0; c<j ;c++){
+      printf("%2.2f\t\t\t", distances[c]);
+      printf("%2.2f\n", noSort[c]);
+    }
 
     sprintf(text, "points ");
     fputs(text, fp_out);
 
-    for(int c = 0; c < j; c++){      
-      for(int a = 0, k = 0; a < j; a++, k += 2){
+    for(int c = 0; c <= j; c++){   
+      k = 0;
+      for(int a = 0; a <= j ; k += 2 , a++){
         if(distances[c] == noSort[a]){
           sprintf(text, "(%d,%d) ", number[k], number[k+1]);
           fputs(text, fp_out);
@@ -98,6 +98,9 @@ float distanceCalc(int x1, int y1, int x2, int y2)
   return distance;
 }
 
+/*
+Função que ordena o vetor
+*/
 void insertionSort(float A[], int length)
 {
      int j;
