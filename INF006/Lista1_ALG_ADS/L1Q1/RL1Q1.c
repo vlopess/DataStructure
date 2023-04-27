fb0a3b019c0f1d8524fc61c577ac653cfc23d0b8
VLopes <victorlopes9291@gmail.com>
2023-04-27 11:25:25 -0300

    {RLQ1} - Iniciando a questão, lendo os números.

--- INF006/Lista1_ALG_ADS/L1Q1/RL1Q1.c
+++ INF006/Lista1_ALG_ADS/L1Q1/RL1Q1.c
@@ -6,49 +6,56 @@
 void insertionSort(float A[], int length);
 
 
-int main(){
+int main()
+{
+
     FILE *fp_in = fopen("L1Q1.in", "r");
     FILE *fp_out = fopen("L1Q1.out", "w");
     
     
-    if(fp_in == NULL || fp_out == NULL){
+    if(fp_in == NULL || fp_out == NULL)
+    {
         printf("Erro na leitura dos arquivos\n");
         return EXIT_FAILURE;
     }
-
     
     char file[255];
     char space[] = " "; 
-    //start 2 4 1 21 start 3 9 18 1 start 3 12 4 12 3
-    while(fgets(file, 255, fp_in) != NULL){
-        
-        char *listNumbers[255];
-        int iListNumbers = 0;
-        
+    //start 2 3 4 56 start 7 6 4 0 1 start 1
+    while(fgets(file, 255, fp_in) != NULL)
+    {
+        // char *listNumbers[255];
+        // int iListNumbers = 0;
+        int tam = strlen(file);
+        if(strchr(file, '\n') != NULL)
+          file[tam - 1] = '\0';
+
+        printf("%s?\n", file);
+
         char *slice  =  strtok(file, space);
+                
 
         while (slice != NULL)
         {
+            printf("(%s)\n", slice);
             int numbers[255];
             int iNumbers = 0;
-            char *copy[255];
+            // char copy[255];
 
             slice  =  strtok(NULL, space);
 
-            while(strcmp(slice, "start") && slice != NULL)
+            while(strcmp(slice, "start") != 0 && slice != NULL)
             {
+                printf("!(%s)\n", slice);
                 numbers[iNumbers++] = atoi(slice);
                 slice  =  strtok(NULL, space);
             }
+            printf("K(%s)\n", slice);
             for (int i = 0; i < iNumbers; i++)
             {
-                printf("%d", numbers[i]);
-                sprintf(copy, "%d ", numbers[i]);
-                fputs(copy, fp_out);
+                printf("%d \n", numbers[i]);
+                fputs("sla",fp_out);
             }
-
-            fputs("sla",fp_out);
-        
         }
         
     }
