0430f332ca2c10f02e3694c05998aeeed73a4f87
VLopes <victorlopes9291@gmail.com>
2023-05-29 23:42:06 -0300

    RL1Q1 - completa

--- INF006/Lista1_ALG_ADS/L1Q1/RL1Q1.c
+++ INF006/Lista1_ALG_ADS/L1Q1/RL1Q1.c
@@ -1,85 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
-#include <math.h>
+#include <ctype.h>
 
-void insertionSort(float A[], int length);
+typedef struct list{
+  int *numbers;
+  int len;
+  int sum;
+  struct list *next;
+}List;
 
+void insertionSort(int *vetor, int tam);
+void putInList(List *novo, List **start);
+void freeMemory(List *start);
 
-int main()
-{
+int main() {
+  FILE *fp_in = fopen("L1Q1.in", "r");
+  FILE *fp_out = fopen("L1Q1.out", "w");
 
-    FILE *fp_in = fopen("L1Q1.in", "r");
-    FILE *fp_out = fopen("L1Q1.out", "w");
-    
-    
-    if(fp_in == NULL || fp_out == NULL)
-    {
-        printf("Erro na leitura dos arquivos\n");
-        return EXIT_FAILURE;
-    }
-    
-    char file[255];
-    char space[] = " "; 
-    //start 2 3 4 56 start 7 6 4 0 1 start 1
-    while(fgets(file, 255, fp_in) != NULL)
-    {
-        // char *listNumbers[255];
-        // int iListNumbers = 0;
-        int tam = strlen(file);
-        if(strchr(file, '\n') != NULL)
-          file[tam - 1] = '\0';
-
-        printf("%s?\n", file);
-
-        char *slice  =  strtok(file, space);
-                
-
-        while (slice != NULL)
-        {
-            printf("(%s)\n", slice);
-            int numbers[255];
-            int iNumbers = 0;
-            // char copy[255];
-
-            slice  =  strtok(NULL, space);
-
-            while(strcmp(slice, "start") != 0 && slice != NULL)
-            {
-                printf("!(%s)\n", slice);
-                numbers[iNumbers++] = atoi(slice);
-                slice  =  strtok(NULL, space);
-            }
-            printf("K(%s)\n", slice);
-            for (int i = 0; i < iNumbers; i++)
-            {
-                printf("%d \n", numbers[i]);
-                fputs("sla",fp_out);
-            }
+  if(fp_in == NULL || fp_out == NULL) {
+    printf("Erro na leitura dos arquivos\n");
+    return EXIT_FAILURE;
+  }
+  char file[255];
+  List *start = NULL;
+  while(fgets(file, 255, fp_in) != NULL){
+    char *slice = strtok(file, " ");
+    while(slice != NULL){
+      if(!strcmp(slice, "start")){
+        List *novo = malloc(sizeof(List));
+        novo->len = 0;
+        novo->sum = 0;
+        novo->next = NULL;
+        novo->numbers = NULL;
+        slice = strtok(NULL, " ");
+        while(slice && strcmp(slice, "start")){         
+          novo->numbers = realloc(novo->numbers, (novo->len + 1) * sizeof(int*));
+          int num = atoi(slice);
+          novo->numbers[novo->len] = num;
+          novo->len++;
+          novo->sum+=num;
+          slice = strtok(NULL, " ");
         }
         
+        insertionSort(novo->numbers, novo->len);
+        putInList(novo, &start);       
+      }
     }
-
-    fclose(fp_in);
-    fclose(fp_out);
-    return EXIT_SUCCESS;
+    for(List *i = start ; i != NULL; i = i->next){
+      fputs("start ", fp_out);
+      int tam = i->len;
+      for(int c = 0 ; c < tam ; c++){
+        char num[10];
+        sprintf(num, "%d ", i->numbers[c]);
+        fputs(num, fp_out);
+      }    
+    }
+    fputc('\n', fp_out);
+    freeMemory(start);
+    start = NULL;    
+  }  
+  return EXIT_SUCCESS;
 }
 
-/*
-Função que ordena o vetor as distancias do vetor
-*/
-void insertionSort(float A[], int length)
-{
-     int j;
-     for (j = 1; j < length; j++)
-     {
-          float key = A[j];
-          int i = j - 1;
-          while (i >= 0 && A[i] > key)
-          {
-              A[i + 1] = A[i];
-              i--;
-          } 
-          A[i + 1] = key;
-     }
+
+void insertionSort(int *vetor, int tam) {
+    int i, chave, j;
+    for (i = 1; i < tam; i++) {
+        chave = vetor[i];
+        j = i - 1;
+        while (j >= 0 && vetor[j] > chave) {
+            vetor[j + 1] = vetor[j];
+            j = j - 1;
+        }
+        vetor[j + 1] = chave;
+    }
+}
+
+void putInList(List *novo, List **start){
+  if(*start == NULL){
+    *start = novo;
+    return;
+  }
+  List *p = *start;
+  List *old = NULL;
+  while(p && p->sum < novo->sum){
+    old = p;
+    p = p->next;
+  }
+  if(p == *start){
+    novo->next = *start;
+    *start = novo;
+    return;
+  }
+  novo->next = old->next;
+  old->next = novo;
+}
+
+void freeMemory(List *start){
+  List *i = start;
+  while(i){
+    if(i->numbers){
+     free(i->numbers); 
+    }      
+    free(i);
+    i = i->next;
+  }
 }
